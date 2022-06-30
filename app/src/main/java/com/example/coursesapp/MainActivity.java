11307bb6331d2b1ce0d5b7cffe67a783e54389a1
Alexey <pro100_lex2005@mail.ru>
2022-06-30 15:51:25 +0900

    Add recycle view for filter

---
+++ app/src/main/java/com/example/coursesapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.coursesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.coursesapp.adapter.CategoryAdapter;
+import com.example.coursesapp.model.Category;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView categoryRecycler;
+    CategoryAdapter categoryAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, "Игры"));
+        categoryList.add(new Category(2, "Сайты"));
+        categoryList.add(new Category(3, "Языки"));
+        categoryList.add(new Category(4, "Прочее"));
+
+        setCategoryRecycler(categoryList);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
+
+    }
+}
